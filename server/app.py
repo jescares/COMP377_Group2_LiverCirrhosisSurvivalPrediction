dd9421d4141e4797f675fba9a6a63359c2c0acd6
jescares <jescares@my.centennialcollege.ca>
2024-04-09 02:03:15 -0400

    tested /predict with sample data

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -13,11 +14,30 @@ model_path = os.path.join(current_directory, 'logistic_regression_model.pkl')
 with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
 
+# Sample data for prediction
+sample_data = {
+    'Bilirubin': [1.0],         # Adjust bilirubin level (higher)
+    'Copper': [100.0],          # Adjust copper level (higher)
+    'Prothrombin': [11.0],      # Adjust prothrombin time (normal)
+    'Stage': [1],               # Adjust stage of disease (early)
+    'Edema_N': [1],             # No edema
+    'Hepatomegaly_N': [1],      # No hepatomegaly
+    'Hepatomegaly_Y': [0],      # No hepatomegaly
+    'Ascites_Y': [0],           # No ascites
+    'Ascites_N': [1],           # No ascites
+    'Alk_Phos': [80.0]          # Adjust alkaline phosphatase level (normal)
+}
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Extract user input from the HTTP request
-    data = request.get_json()
-    user_input = np.array(data['user_input']).reshape(1, -1)
+    # Extract user input from the HTTP request (not used in this case)
+    # data = request.get_json()
+
+    # Prepare sample data for prediction
+    new_sample_data = pd.DataFrame(sample_data)
+
+    # Preprocess the sample data for prediction (reshape if necessary)
+    user_input = new_sample_data.values
 
     # Make prediction using the loaded logistic regression model
     prediction = model.predict(user_input)
